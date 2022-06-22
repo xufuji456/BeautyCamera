b8d686cf8e06d6374b27c13dccbd89c32ab0f1f9
xufulong <xufl@flatincbr.com>
2022-06-22 10:39:25 +0800

    添加BeautyPixarFilter

---
+++ BeautyFilter/src/main/java/com/frank/beautyfilter/filter/advance/BeautyPixarFilter.java
@@ -0,0 +1,68 @@
+package com.frank.beautyfilter.filter.advance;
+
+import android.opengl.GLES20;
+
+import com.frank.beautyfilter.R;
+import com.frank.beautyfilter.filter.base.gpuimage.GPUImageFilter;
+import com.frank.beautyfilter.util.BeautyParams;
+import com.frank.beautyfilter.util.OpenGLUtil;
+
+import java.util.Arrays;
+
+/**
+ * @author xufulong
+ * @date 2022/6/22 10:27 上午
+ * @desc
+ */
+public class BeautyPixarFilter extends GPUImageFilter {
+
+    private int strengthLocation;
+    private final int[] inputTextureHandle = {-1};
+    private final int[] inputTextureUniformLocation = {-1};
+
+    public BeautyPixarFilter() {
+        super(NORMAL_VERTEX_SHADER, OpenGLUtil.readShaderFromSource(R.raw.pixar));
+    }
+
+    public void onInit() {
+        super.onInit();
+        for (int i=0; i<inputTextureUniformLocation.length; i++) {
+            strengthLocation = GLES20.glGetUniformLocation(getProgramId(), "strength");
+            inputTextureUniformLocation[i] = GLES20.glGetUniformLocation(getProgramId(), "inputImageTexture" + (i+2));
+        }
+    }
+
+    public void onInitialized() {
+        super.onInitialized();
+        setFloat(strengthLocation, 1.0f);
+        runOnDraw(new Runnable() {
+            @Override
+            public void run() {
+                inputTextureHandle[0] = OpenGLUtil.loadTexture(BeautyParams.context, "filter/pixar_curves.png");
+            }
+        });
+    }
+
+    protected void onDrawArraysPre() {
+        for (int i=0; i<inputTextureHandle.length && inputTextureHandle[i] != OpenGLUtil.NO_TEXTURE; i++) {
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0 + (i+3));
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, inputTextureHandle[i]);
+            GLES20.glUniform1i(inputTextureUniformLocation[i], i+3);
+        }
+    }
+
+    protected void onDrawArraysAfter() {
+        for (int i=0; i<inputTextureHandle.length && inputTextureHandle[i] != OpenGLUtil.NO_TEXTURE; i++) {
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0 + (i+3));
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0); //TODO
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        GLES20.glDeleteTextures(1, inputTextureHandle, 0);
+        Arrays.fill(inputTextureHandle, OpenGLUtil.NO_TEXTURE);
+    }
+
+}
--- BeautyFilter/src/main/java/com/frank/beautyfilter/filter/factory/BeautyFilterFactory.java
+++ BeautyFilter/src/main/java/com/frank/beautyfilter/filter/factory/BeautyFilterFactory.java
@@ -3,6 +3,7 @@ package com.frank.beautyfilter.filter.factory;
 import com.frank.beautyfilter.filter.advance.BeautyCoolFilter;
 import com.frank.beautyfilter.filter.advance.BeautyImageAdjustFilter;
 import com.frank.beautyfilter.filter.advance.BeautyLatteFilter;
+import com.frank.beautyfilter.filter.advance.BeautyPixarFilter;
 import com.frank.beautyfilter.filter.advance.BeautyRomanceFilter;
 import com.frank.beautyfilter.filter.advance.BeautySketchFilter;
 import com.frank.beautyfilter.filter.advance.BeautySunriseFilter;
