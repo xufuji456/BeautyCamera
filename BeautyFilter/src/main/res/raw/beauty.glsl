4d71f62d1832e3bfe5935ee52ee1510dc408dbdf
xufulong <xufl@flatincbr.com>
2023-05-01 11:25:00 +0800

    update beauty.glsl

--- BeautyFilter/src/main/res/raw/beauty.glsl
+++ BeautyFilter/src/main/res/raw/beauty.glsl
@@ -6,7 +6,7 @@ uniform sampler2D inputImageTexture;
 uniform vec2 singleStepOffset;
 uniform mediump float params;
 
-const highp vec3 W = vec3(0.299,0.587,0.114);
+const highp vec3 W = vec3(0.299, 0.587, 0.114);
 vec2 blurCoordinates[20];
 
 float hardLight(float color)
@@ -20,17 +20,17 @@ float hardLight(float color)
 
 void main(){
 
-    vec3 centralColor = texture2D(inputImageTexture, textureCoordinate).rgb;
-    blurCoordinates[0] = textureCoordinate.xy + singleStepOffset * vec2(0.0, -10.0);
-    blurCoordinates[1] = textureCoordinate.xy + singleStepOffset * vec2(0.0, 10.0);
-    blurCoordinates[2] = textureCoordinate.xy + singleStepOffset * vec2(-10.0, 0.0);
-    blurCoordinates[3] = textureCoordinate.xy + singleStepOffset * vec2(10.0, 0.0);
-    blurCoordinates[4] = textureCoordinate.xy + singleStepOffset * vec2(5.0, -8.0);
-    blurCoordinates[5] = textureCoordinate.xy + singleStepOffset * vec2(5.0, 8.0);
-    blurCoordinates[6] = textureCoordinate.xy + singleStepOffset * vec2(-5.0, 8.0);
-    blurCoordinates[7] = textureCoordinate.xy + singleStepOffset * vec2(-5.0, -8.0);
-    blurCoordinates[8] = textureCoordinate.xy + singleStepOffset * vec2(8.0, -5.0);
-    blurCoordinates[9] = textureCoordinate.xy + singleStepOffset * vec2(8.0, 5.0);
+    vec3 inputColor = texture2D(inputImageTexture, textureCoordinate).rgb;
+    blurCoordinates[0]  = textureCoordinate.xy + singleStepOffset * vec2(0.0, -10.0);
+    blurCoordinates[1]  = textureCoordinate.xy + singleStepOffset * vec2(0.0, 10.0);
+    blurCoordinates[2]  = textureCoordinate.xy + singleStepOffset * vec2(-10.0, 0.0);
+    blurCoordinates[3]  = textureCoordinate.xy + singleStepOffset * vec2(10.0, 0.0);
+    blurCoordinates[4]  = textureCoordinate.xy + singleStepOffset * vec2(5.0, -8.0);
+    blurCoordinates[5]  = textureCoordinate.xy + singleStepOffset * vec2(5.0, 8.0);
+    blurCoordinates[6]  = textureCoordinate.xy + singleStepOffset * vec2(-5.0, 8.0);
+    blurCoordinates[7]  = textureCoordinate.xy + singleStepOffset * vec2(-5.0, -8.0);
+    blurCoordinates[8]  = textureCoordinate.xy + singleStepOffset * vec2(8.0, -5.0);
+    blurCoordinates[9]  = textureCoordinate.xy + singleStepOffset * vec2(8.0, 5.0);
     blurCoordinates[10] = textureCoordinate.xy + singleStepOffset * vec2(-8.0, 5.0);
     blurCoordinates[11] = textureCoordinate.xy + singleStepOffset * vec2(-8.0, -5.0);
     blurCoordinates[12] = textureCoordinate.xy + singleStepOffset * vec2(0.0, -6.0);
@@ -42,41 +42,41 @@ void main(){
     blurCoordinates[18] = textureCoordinate.xy + singleStepOffset * vec2(4.0, -4.0);
     blurCoordinates[19] = textureCoordinate.xy + singleStepOffset * vec2(4.0, 4.0);
 
-    float sampleColor = centralColor.g * 20.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[0]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[1]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[2]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[3]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[4]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[5]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[6]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[7]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[8]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[9]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[10]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[11]).g;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[12]).g * 2.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[13]).g * 2.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[14]).g * 2.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[15]).g * 2.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[16]).g * 2.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[17]).g * 2.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[18]).g * 2.0;
-    sampleColor += texture2D(inputImageTexture, blurCoordinates[19]).g * 2.0;
+    float blurColor = inputColor.g * 20.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[0]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[1]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[2]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[3]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[4]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[5]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[6]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[7]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[8]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[9]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[10]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[11]).g;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[12]).g * 2.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[13]).g * 2.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[14]).g * 2.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[15]).g * 2.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[16]).g * 2.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[17]).g * 2.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[18]).g * 2.0;
+    blurColor += texture2D(inputImageTexture, blurCoordinates[19]).g * 2.0;
 
-    sampleColor = sampleColor / 48.0;
+    blurColor = blurColor / 48.0;
 
-    float highPass = centralColor.g - sampleColor + 0.5;
+    float highPass = inputColor.g - blurColor + 0.5;
 
     for(int i = 0; i < 5;i++)
     {
         highPass = hardLight(highPass);
     }
-    float luminance = dot(centralColor, W);
+    float luminance = dot(inputColor, W);
 
     float alpha = pow(luminance, params);
 
-    vec3 smoothColor = centralColor + (centralColor-vec3(highPass))*alpha*0.1;
+    vec3 smoothColor = inputColor + (inputColor - vec3(highPass)) * alpha * 0.1;
 
-    gl_FragColor = vec4(mix(smoothColor.rgb, max(smoothColor, centralColor), alpha), 1.0);
+    gl_FragColor = vec4(mix(smoothColor.rgb, max(smoothColor, inputColor), alpha), 1.0);
 }
